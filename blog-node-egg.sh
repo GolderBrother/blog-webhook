1afe8ca78cc9146fc2420a4f2718b4d446bf229e
耀煌 <yaohuang.zhang@zkteco.com>
2019-08-31 20:40:43 +0800

    update shell file

--- blog-node-egg.sh
+++ blog-node-egg.sh
@@ -1,6 +1,6 @@
-# !/bin/bash
+#!/bin/bash
 # 进入到服务端工作目录
-WORK_PATH '/usr/projects/blog/blog-node-egg'
+WORK_PATH='/usr/projects/blog/blog-node-egg'
 cd $WORK_PATH
 echo '清除老代码'
 # 回退版本，历史区回退到暂存区
@@ -21,5 +21,5 @@ echo '启动新容器'
 # 宿主机的端口映射到docker容器的6100端口，前提是docker容器要暴露端口出来
 # -d:后台运行，不堵塞当前命令行窗口
 # blog-node-egg: 镜像名字（基于blog-node-egg镜像启动服务，后台运行）
-docker container run -p 6000:6000 -d blog-node-egg:1.0
+docker container run -p 6000:6000 --name blog-node-egg-container -d blog-node-egg:1.0
 
